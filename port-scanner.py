bd026c9f6bc9bd790ac386d10aa9e20522050e5b
willspader <spaderwilliam@hotmail.com>
2020-05-06 22:31:40 -0300

    domain

--- port-scanner.py
+++ port-scanner.py
@@ -1,23 +1,23 @@
 import socket, threading
 
 """ Check if a given port is open (TCP) """
-def isPortOpen(port, output):
+def isPortOpen(port, output, domain):
     tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     tcpSocket.settimeout(2)
 
     try:
-        output[port] = tcpSocket.connect_ex(("www.python.org", port))
+        output[port] = tcpSocket.connect_ex((domain, port))
     except:
         pass
 
 """ starts the thread scanning for all ports, It creates one instance for each port. Range of ports = [1, 10000] """
-def threadScanning():
+def threadScanning(domain):
     threads = [] # store all threads
     output = {} # store the return of the method isPortOpen
 
     for i in range(10001):
-        thread = threading.Thread(target=isPortOpen, args=(i, output))
+        thread = threading.Thread(target=isPortOpen, args=(i, output, domain))
         threads.append(thread)
         thread.start()
 
@@ -29,4 +29,4 @@ def threadScanning():
             print(f'port {i} is open')
 
 if __name__ == '__main__':
-    threadScanning()
+    threadScanning("www.python.org")
